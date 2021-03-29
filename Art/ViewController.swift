6701c51270b5c9efce8800c5fab586e50e00514d
Alexa Fernandez <alexafernn@Alexas-MacBook-Air.local>
2021-03-29 17:52:04 -0400

    inital repo

---
+++ Art/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Art
+//
+//  Created by Alexa on 2/10/21.
+//  Copyright © 2021 Alexa. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+//    stuct ContentView:View{
+//    var body: some View
+//    {
+//    Image("CodeArt")
+//    .resizable()
+//    .scaledToFit
+//    }
+//    
+//    override var preferredStatusBarStyle: UIStatusBarStyle{
+//        return .lightContent
+//    }
+
+
+}
+
