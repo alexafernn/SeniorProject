ecd22a7d9a2208ddd370bd9f28a4ae58dcaaa0c6
Alexa Fernandez <alexafernn@Alexas-MacBook-Air.local>
2021-04-21 23:17:33 -0400

    fixed transition

--- Art/SecondViewController.swift
+++ Art/SecondViewController.swift
@@ -7,10 +7,29 @@
 //
 
 import UIKit
+import Alamofire
 
 /*register view controller*/
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController
+{
+    @IBOutlet var _firstname: UITextField!
+    @IBOutlet var _lastname: UITextField!
+    @IBOutlet var _email: UITextField!
+    @IBOutlet var _password: UITextField!
+    @IBOutlet var _graduationyear: UITextField!
+    @IBOutlet var _birthdate: UITextField!
+    @IBOutlet var _gender: UITextField!
+    @IBOutlet var _learnaboutca: UITextField!
 
+//    @IBOutlet var _firstname: UITextField!
+//    @IBOutlet var _lastname: UITextField!
+//    @IBOutlet var _email: UITextField!
+//    @IBOutlet var _password: UITextField!
+//    @IBOutlet var _graduationyear: UITextField!
+//    @IBOutlet var _birthdate: UITextField!
+//    @IBOutlet var _gender: UITextField!
+//    @IBOutlet var _learnedAboutCodeArt: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,7 +43,43 @@ class SecondViewController: UIViewController {
     
     @IBAction func didSignUp()
     {
-        self.dismiss(animated: true, completion: nil);
+        let firstname = _firstname.text
+        let lastname = _lastname.text
+        let email = _email.text
+        let password = _password.text
+        let graduationyear = _graduationyear.text
+        let birthdate = _birthdate.text
+        let gender = _gender.text
+        let learnaboutca = _learnaboutca.text
+        
+        //testing
+        print(firstname, lastname, email, password, graduationyear, birthdate, gender,learnaboutca)
+
+        let params = ["is_admin": false, "is_student":true, "first_name": firstname, "last_name": lastname, "email": email, "graduation": graduationyear, "birthday": birthdate, "gender": gender, "attributes": learnaboutca, "password": password] as [String : Any]
+        
+        let url = URL(string: "http://codeart.cs.loyola.edu/accountcreate")!
+        let h: HTTPHeaders = ["Content-Type": "application/json"]
+        
+        AF.request(url, method: .post, parameters: params, encoding: JSONEncoding.default, headers:h)
+            .validate(statusCode: 200..<300)
+                        /*.responseJson or .responseString*/
+                            .responseJSON { response in
+                                debugPrint("PRINTING DEBUG: ", response)
+                                print("response is " , response.response!.statusCode)
+                                switch response.result
+                                {
+                                    case .success(let data):
+                                        print(response)
+                                        print("IN SUCCESS")
+                                        self.dismiss(animated: true, completion: nil);
+                                        
+                                    case .failure(let error):
+                                        print("error is ", error)
+                             }
+                        }
+        
+
+        //self.dismiss(animated: true, completion: nil);
         //need to save the stuff to the database 
     }
     
--- Art/ViewController.swift
+++ Art/ViewController.swift
@@ -16,7 +16,6 @@ class ViewController: UIViewController
     @IBOutlet var _username: UITextField!
     @IBOutlet var _password: UITextField!
     @IBOutlet var _login_button: UIButton!
-//    var goodAuth = true;
     
     override func viewDidLoad()
     {
@@ -48,65 +47,23 @@ class ViewController: UIViewController
         let vc = storyboard?.instantiateViewController(withIdentifier: "second_vc") as! SecondViewController
         present(vc,animated: true)
     }
+
     
     
 
     @IBAction func LoginButton(_ sender: Any)
     {
-//        if(_login_button.titleLabel?.text == "Logout")
-//        {
-//            let preferences = UserDefaults.standard
-//            preferences.removeObject(forKey:"session")
-//
-//            LoginToDo()
-//            return
-//        }
-//
-        
         let username = _username.text
         let password = _password.text
-        
-        //if enters no user name then it wont allow u to go on
-//        if(username == "" || password == "")
-//        {
-//            return
-//        }
-        
-        //DoLogin2(username!, password!)
-        //gonna need some type of if statement here
-        // print("good auth before")
-        //print(goodAuth)
-        //DoLogin3(username!, password!)
-        //print(goodAuth)
-       // if (goodAuth == true)
-      //  {
-//            let vc3 = storyboard?.instantiateViewController(withIdentifier: "test") as! UINavigationController
-//            present(vc3, animated:true )
-//        }
-//
-        
+
                 
         DoLogin4(username!, password!)
-//        print("getting to this point")
-//        if(goodAuth == true)
-////        let vc3 = storyboard?.instantiateViewController(withIdentifier: "third_vc") as! ThirdViewController
-//        {
-//            print("good auth = true")
-//            let vc3 = storyboard?.instantiateViewController(withIdentifier: "test") as! UINavigationController
-//            present(vc3, animated:true )
-//        }
-//        else
-//        {
-//            print("good auth = false")
-//        }
     }
     
     func DoLogin4(_ user:String, _ psw: String)
     {
         let username = user
         let password = psw
-        //let header = ["Content-Type" : "application/json"]
-        var goodAuth = false
         
         print("username is: ")
         print(username)
@@ -115,7 +72,6 @@ class ViewController: UIViewController
         
         if (username == "" || password == "")
         {
-            goodAuth = false
             print("entered no user and/or password")
             let missingCredenital = UIAlertController(title: "Sign in Failed!", message: "Please enter Username and Password", preferredStyle: UIAlertController.Style.alert)
             missingCredenital.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
@@ -141,14 +97,11 @@ class ViewController: UIViewController
                                             print(response)
                                             print("IN SUCCESS")
                                             print("isi: \(data)")
-                                            
-                                            goodAuth = true
                                             let vc3 = self.storyboard?.instantiateViewController(withIdentifier: "test") as! UINavigationController
                                             self.present(vc3, animated:true )
                                             
                                         case .failure(let error):
                                             print("error is ", error)
-                                            goodAuth = false
                                             let wrongLogin = UIAlertController(title: "Sign in Failed!", message: "Please enter correct Username and Password", preferredStyle: UIAlertController.Style.alert)
                                             wrongLogin.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
                                                     self.dismiss(animated: true, completion: nil)
@@ -159,199 +112,9 @@ class ViewController: UIViewController
                             }
             
             }
-//        print("made it out of else")
-//        if(goodAuth==true)
-//        {
-//            let vc3 = storyboard?.instantiateViewController(withIdentifier: "test") as! UINavigationController
-//            present(vc3, animated:true )
-//        }
     }
     
-    //trying out new approach:
-    func DoLogin3(_ user:String, _ psw: String)
-    {
-        let username = user
-        let password = psw
-        var goodAuth = false
-        
-        print("username is: ")
-        print(username)
-        print("password is: ")
-        print(password)
-           
-        if (username == "" || password == "")
-        {
-            print("entered no user or password")
-            let missingCredenital = UIAlertController(title: "Sign in Failed!", message: "Please enter Username and Password", preferredStyle: UIAlertController.Style.alert)
-            missingCredenital.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
-                    self.dismiss(animated: true, completion: nil)
-                }))
-                self.present(missingCredenital, animated: true, completion: nil)
-        }
-        else
-        {
-            print("in else , they entered login credentials")
-            let parameters = ["email": username, "password": password]
-            let url = URL(string: "http://codeart.cs.loyola.edu/account/login")!
-            let session = URLSession.shared
-            
-            //now create the URLRequest object using the url object
-            var request = URLRequest(url: url)
-            request.httpMethod = "POST" //set http method as POST
-
-            do
-            {
-                print("inside do")
-                request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
-            }
-            catch let error
-            {
-                print("inside error")
-                print(error.localizedDescription)
-            }
-
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
-
-            //create dataTask using the session object to send data to the server
-            let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-
-                guard error == nil else {
-                    return
-                }
-
-                guard let data = data else {
-                    return
-                }
-
-                do {
-                    //create json object from data
-                    if let json = try JSONSerialization.jsonObject(with: data, options: /*.mutableContainers,*/ .allowFragments) as? [String: Any] {
-                        print("inside if/do")
-                        print(json)
-                        // handle json...
-                        print("goodAuth in if/do = ", goodAuth)
-                        
-                    }
-                } catch let error {
-                    print("inside catch")
-                    goodAuth = false
-                    print("good auth =", goodAuth)
-                    print("error is ", error)
-                    print(error.localizedDescription)
-                }
-            })
-          //  print("at the end of login, goodAuth = ", goodAuth)
-            task.resume()
-        }
-       
-    }
- 
-    
-    func DoLogin2(_ user:String, _ psw: String)
-    {
-        //let url = URL(string: "http://codeart.cs.loyola.edu/admin")!
-        //declare parameter as a dictionary which contains string as key and value combination. considering inputs are valid
-
-          let parameters = ["email": "a", "password": "a"]
-
-          //create the url with URL
-          let url = URL(string: "http://codeart.cs.loyola.edu/login")! //change the url
-
-          //create the session object
-          let session = URLSession.shared
-
-          //now create the URLRequest object using the url object
-          var request = URLRequest(url: url)
-          request.httpMethod = "POST" //set http method as POST
-
-          do {
-              request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted) // pass dictionary to nsdata object and set it as request body
-          } catch let error {
-              print(error.localizedDescription)
-          }
-
-          request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-          request.addValue("application/json", forHTTPHeaderField: "Accept")
-
-          //create dataTask using the session object to send data to the server
-          let task = session.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
-
-              guard error == nil else {
-                  return
-              }
-
-              guard let data = data else {
-                  return
-              }
-
-              do {
-                  //create json object from data
-                  if let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: Any] {
-                      print(json)
-                      // handle json...
-                  }
-              } catch let error {
-                  print(error.localizedDescription)
-              }
-          })
-          task.resume()
-    }
-    
-    func DoLogin(_ user:String, _ psw:String)
-    {
-        print("inside do login ")
-        let url = URL(string:"http://codeart.cs.loyola.edu/login")
-        let session = URLSession.shared
-        
-        let request = NSMutableURLRequest(url:url!)
-        request.httpMethod = "POST"
-        
-        let paramToSend = "email=" + user + "&password=" + psw
-        
-        request.httpBody = paramToSend.data(using: .utf8)
-         
-        let task = session.dataTask(with: request as URLRequest, completionHandler:
-            {
-                (data, response, error) in
-                
-                guard let _:Data = data else
-                {
-                    return
-                }
-                let json:Any?
-                
-                do
-                {
-                    json = try JSONSerialization.jsonObject(with: data!, options: [])
-                }
-                catch
-                {
-                   // self.goodAuth = false
-                    json = nil
-                }
-                
-                guard let server_response = json as? NSDictionary else
-                {
-                    return
-                }
-                
-                if let data_block = server_response["data"] as? NSDictionary
-                {
-                    if let session_data = data_block["session"] as? String
-                    {
-                        let preferences = UserDefaults.standard
-                        preferences.set(session_data,forKey:"session")
-                        
-                        DispatchQueue.main.async(
-                            execute:self.LoginDone
-                        )
-                    }
-                }
-            })
-        
-        task.resume()
-    }
+   
     
     func LoginToDo()
     {
