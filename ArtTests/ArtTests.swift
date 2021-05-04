9e0b27e14ece19f1149a92f3a219986a8a0e9e49
Alexa Fernandez <alexafernn@Alexas-MacBook-Air.local>
2021-05-03 23:16:26 -0400

    final updates senior capstone

---
+++ ArtTests/ArtTests.swift
@@ -0,0 +1,108 @@
+//
+//  ArtTests.swift
+//  ArtTests
+//
+//  Created by Alexa Fernandez on 5/3/21.
+//  Copyright © 2021 Alexa. All rights reserved.
+//
+
+//import XCTest
+//@testable import Art
+
+//class ArtTests: XCTestCase {
+  
+    
+//    func testLoginButton_IsWorking()
+//    {
+//        XCTAssert()
+//    }
+    
+//    var graduationYear: String?{
+//        didSet
+//            graduationyear = String()
+//    }
+//
+//    var gender: String?{
+//        didSet
+//            gender = String()
+//    }
+//
+//    var learnaboutCA: String?{
+//        didSet
+//            learnaboutCar = String()
+//    }
+//
+//    func testLoginButton_Working()
+//    {
+//        XCTAssert(hasValue(viewController._login_button))
+//
+//    }
+//    func testForgotPassword_Working()
+//    {
+//        XCTAssert(hasValue(viewController.didTapForgotPassword()))
+//
+//    }
+//
+//    func testRegister()
+//    {
+//        XCTAssert(hasValue(viewController.didTapRegister()()))
+//
+//    }
+//
+//    func didGender()
+//    {
+//        XCTAssert(hasValue(SecondViewController.didGender()()))
+//
+//    }
+//
+//    func didLearnCA()
+//    {
+//        XCTAssert(hasValue(SecondViewController.didlearnaboutCA()()))
+//
+//    }
+//
+//    func didGrad()
+//    {
+//        XCTAssert(hasValue(SecondViewController.didGradYear()()))
+//
+//    }
+//
+//    func didCancel()
+//    {
+//        XCTAssert(hasValue(SecondViewController.didCancel()()))
+//
+//    }
+//    func didMenuItem()
+//    {
+//        XCTAssert(hasValue(ThirdViewController.didSelectMenuItem()))
+//
+//    }
+//
+//    func didEvents()
+//    {
+//        XCTAssert(hasValue(ThirdViewController.didTapEvents()))
+//
+//    }
+//
+//    func didInternships()
+//    {
+//        XCTAssert(hasValue(ThirdViewController.didTapInternships()))
+//
+//    }
+
+//    var count: Int?{
+//        didSet
+//           count = Int()
+//    }
+//
+//    var company_arr: [String]?{
+//        didSet
+//        company_arr = [String]()
+//    }
+//
+//    var role_arr: [String]?{
+//        didSet
+//        role_arr = [String]()
+//    }
+
+//}
